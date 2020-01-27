e82911f58b48a253d6e52a74d4e7f43e920548c6
Souman Paul <souman1729@gmail.com>
2020-01-27 23:25:54 +0530

    added test case in my blog app

--- database/config/config.js
+++ database/config/config.js
@@ -11,7 +11,7 @@ module.exports = {
   test: {
     username: "postgres",
     password: "postgres",
-    database: "test_db",
+    database: "dev_test",
     host: "127.0.0.1",
     dialect: "postgres",
   },
